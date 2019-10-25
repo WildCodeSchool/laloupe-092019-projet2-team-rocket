59ecbdd6a0fe581095155fc9696901111a000746
Mathieu GAUBAL-VATILINGON <b_jayson@hotmail.com>
2019-10-25 10:56:16 +0200

    LauncherPage finished

--- discover-the-universe/src/App.js
+++ discover-the-universe/src/App.js
@@ -1,11 +1,16 @@
 import React, { Component} from 'react';
 import LauncherPage from './Components/LauncherPage/LauncherPage';
-
+import LauncherSettings from './Components/LauncherSettings/LauncherSettings';
+import Footer from './Components/Footer/Footer';
+import Nav from './Components/Nav/NavBar';
 class App extends Component{
     render(){
         return(
             <div>
+                <Nav />
                 <LauncherPage />
+                <Footer />
+
             </div>
         )
     }
--- discover-the-universe/src/Components/LauncherPage/LauncherPage.jsx
+++ discover-the-universe/src/Components/LauncherPage/LauncherPage.jsx
@@ -1,15 +1,14 @@
 import React, {Component} from 'react';
 import LauncherButton from './LauncherButton/LauncherButton';
 import './LauncherPage.css';
+import '../../Grid.css';
 
 class LauncherPage extends Component{
 
     render(){
         return(
-            <div>
-                
-                <LauncherButton />
-                 
+            <div className="section1">
+                <LauncherButton /> 
             </div>
         )
     }
